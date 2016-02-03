aa87e624c438f443e5b9888d3b432f05a94158b3
namtax <namtax@hotmail.com>
2016-02-03 17:30:25 +0000

    funding: add bin file

---
+++ lib/funding.rb
@@ -0,0 +1,10 @@
+module Funding
+  require 'slop'
+  require 'terminal-table'
+  require 'core_ext/numeric'
+  require 'funding/table'
+  require 'funding/prime'
+  require 'funding/row'
+  require 'funding/terminal_table'
+  require 'funding/cli'
+end
